a20c80b6bb8216b79ee6e86a20b7a83288c1fde5
reshsingh1995@gmail.com <reshsingh1995@gmail.com>
2022-04-12 00:12:20 +0530

    Welcome to Address Book

---
+++ src/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,28 @@
+package com.bridgelabz;
+
+/**
+ * import class
+ */
+
+
+/**
+ * create a class name as AddressBookMain
+ */
+public class AddressBookMain {
+    /**
+     * create a main method all program execute in main method
+     *
+     * @param args no argument
+     */
+    public static void main(String[] args) {
+
+
+        while (true) {
+            /**
+             * 1st display welcome msg
+             * then display al the option
+             */
+            System.out.println("\nWelcome to Address Book System");
+        }
+    }
+}
